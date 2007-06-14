35124ee46bb7a3f842c8a658dd621c014ceff8e2
(no author) <(no author)@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2007-06-14 15:13:53 +0000

    Continuing subdomain work.

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,23 +1,33 @@
 class LoginController < ApplicationController
+
   # Be sure to include AuthenticationSystem in Application Controller instead
   include AuthenticatedSystem
   # If you want "remember me" functionality, add this before_filter to Application Controller
   before_filter :login_from_cookie
 
   def index
-    return unless request.post?
-    self.current_user = User.authenticate(params[:login], params[:password])
-    if logged_in?
-      if params[:remember_me] == "1"
-        self.current_user.remember_me
-        cookies[:auth_token] = { :value => self.current_user.remember_token , :expires => self.current_user.remember_token_expires_at }
+    if request.post?
+      self.current_user = User.authenticate(params[:login], params[:password])
+      if logged_in?
+        if params[:remember_me] == "1"
+          self.current_user.remember_me
+          cookies[:auth_token] = { :value => self.current_user.remember_token , :expires => self.current_user.remember_token_expires_at }
+        end
+        redirect_back_or_default(:controller => '/home', :action => 'index')
+      # Send them back to the login page with appropriate error message
+      else
+        flash[:message] = 'Please specify a valid username and password.'
+        flash[:username] = params[:username]
+        redirect_to '/login'
       end
-      redirect_back_or_default(:controller => '/home', :action => 'index')
-    # Send them back to the login page with appropriate error message
+    # Display the appropriate login form based on subdomain
     else
-      flash[:message] = 'Please specify a valid username and password.'
-      flash[:username] = params[:username]
-      redirect_to '/login'
+      @account = Account.find_by_subdomain(request.subdomains.first)
+      
+      # Subdomain does not exist so display appropriate form
+      if !@account
+        render_text 'this subdomain does not exist, click here to create your account'        
+      end
     end
   end
 
@@ -38,9 +48,5 @@ class LoginController < ApplicationController
     reset_session
     flash[:notice] = "You have been logged out."
     redirect_back_or_default(:controller => '/login', :action => 'index')
-  end
-  
-  def password
-    
   end
 end
