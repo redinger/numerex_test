fee39b70c1f5966a5bb12736604267293bd39648
(no author) <(no author)@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2007-06-18 19:06:49 +0000

    Changed readings to order descending.

--- app/controllers/readings_controller.rb
+++ app/controllers/readings_controller.rb
@@ -9,7 +9,7 @@ class ReadingsController < ApplicationController
   
   # Display last N readings for given device
   def last
-    readings = Reading.find(:all, :conditions => ["device_id = ?", params[:id]], :limit => 25)
+    readings = Reading.find(:all, :conditions => ["device_id = ?", params[:id]], :limit => 25, :order => "created_at desc")
     render_xml readings.to_xml
   end
 end
