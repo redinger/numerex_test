b8560a32bae66e1951176c18e404effaefdf5ae5
(no author) <(no author)@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2007-06-19 16:26:49 +0000

    Defaulted provision status to zero.

--- db/migrate/002_add_provision_status_to_devices_table.rb
+++ db/migrate/002_add_provision_status_to_devices_table.rb
@@ -1,6 +1,6 @@
 class AddProvisionStatusToDevicesTable < ActiveRecord::Migration
   def self.up
-    add_column :devices, :provision_status_id, :integer, :limit => 2
+    add_column :devices, :provision_status_id, :integer, :limit => 2, :default => 0
   end
 
   def self.down
