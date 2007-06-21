0b30fd73e2cf1d0179fd538aa0d491852b71dce4
(no author) <(no author)@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2007-06-21 19:00:00 +0000

    Removing device restfulness for now.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :devices
+  # map.resources :devices
   # The priority is based upon order of creation: first created -> highest priority.
   
   # Sample of regular route:
