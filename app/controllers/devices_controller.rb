c4913f735b770b0a760b04736419ce52889a633d
(no author) <(no author)@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2007-06-21 22:49:28 +0000

    Start of development for adding a device.

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -1,25 +1,17 @@
 class DevicesController < ApplicationController
   #scaffold :device
   
-  # GET /devices /devices.xml
   def index
     @devices = Device.find(:all)
-    
-    respond_to do |format|
-      format.html # index.rhtml
-      format.xml {render :xml => @devices.to_xml}
-    end
   end
   
-  # GET /devices/1 /devices/1.xml
-  def show
-    @device = Device.find(params[:id])
-  
-    respond_to do |format|
-      format.html # show.rhtml
-      format.xml {render :xml => @device.to_xml}
+  # User chooses a device to add
+  # 1 Mobile phone tracker
+  # 2 Personal tracker
+  # 3 Fleet tracker
+  def choose
+    if params[:id] == '3'
+      
     end
   end
-  
-  
 end
