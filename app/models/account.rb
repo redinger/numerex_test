027b59ece7506be76bfc80679151d056439bbe66
(no author) <(no author)@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2007-06-22 19:16:30 +0000

    Association of devices to accounts.

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,3 +1,4 @@
 class Account < ActiveRecord::Base
+  has_many :devices
   has_many :users
 end
--- app/models/device.rb
+++ app/models/device.rb
@@ -1,3 +1,8 @@
 class Device < ActiveRecord::Base
+  belongs_to :account
   has_many :readings, :order => "created_at desc", :limit => 10
+  
+  def self.get_devices
+    find(:all, :conditions => ['provision_status_id = 1'])
+  end
 end
