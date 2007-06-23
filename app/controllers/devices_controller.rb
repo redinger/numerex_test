80b3a600ef82aa5b0462c9875004933942a82d33
salad <salad@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2007-06-23 17:20:58 +0000

    Provisioning stuff.  Assuming device exists in system and is associated to account via user-defined IMEI.

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -9,9 +9,33 @@ class DevicesController < ApplicationController
   # 1 Mobile phone tracker
   # 2 Personal tracker
   # 3 Fleet tracker
+  
+  # A device can provisioned
   def choose
-    if params[:id] == '3'
-      
+    # User is associating device with account via IMEI
+    if request.post?
+      if(params[:imei] != '')
+        device = Device.find_by_imei(params[:imei])
+        # Found the device
+        if(device)
+          if(device.provision_status_id == 0)
+            device.account_id = session[:account_id]
+            imei = params[:imei]
+            device.name = 'Device ' + imei.slice(1, 4)
+            device.provision_status_id = 1
+            device.save
+            flash[:message] = 'Your device was added successfully with a default name of ' + params[:imei]
+            redirect_to :controller => 'devices'
+          else
+            flash[:message] = 'This device has already been provisioned'
+          end
+        # No device matching imei exists
+        else
+          flash[:message] = "We're sorry, there's no device with this IMEI currently in our system"
+          flash[:imei] = params[:imei]
+        end
+      end
     end
   end
+  
 end
