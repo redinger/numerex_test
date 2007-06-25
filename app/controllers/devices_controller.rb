1c630b602921debf72de4e800b2b63b3b97d8cc0
(no author) <(no author)@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2007-06-25 17:50:04 +0000

    Device provisioning work.  A user can provision a device that doesn't yet exist in the system.

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -15,26 +15,46 @@ class DevicesController < ApplicationController
     # User is associating device with account via IMEI
     if request.post?
       if(params[:imei] != '')
-        device = Device.find_by_imei(params[:imei])
-        # Found the device
+        device = Device.find_by_imei(params[:imei]) # Determine if device is already in system
+        
+        # Device is already in the system so let's associate it with this account
         if(device)
           if(device.provision_status_id == 0)
             device.account_id = session[:account_id]
             imei = params[:imei]
-            device.name = 'Device ' + imei.slice(1, 4)
+            device.name = params[:name]
             device.provision_status_id = 1
             device.save
-            flash[:message] = 'Your device was added successfully with a default name of ' + params[:imei]
+            flash[:message] = 'The device "' + params[:name] + '" was provisioned successfully'
             redirect_to :controller => 'devices'
           else
-            flash[:message] = 'This device has already been provisioned'
+            flash[:message] = 'This device has already been added'
           end
-        # No device matching imei exists
+        # No device with this IMEI exists so let's add it
         else
-          flash[:message] = "We're sorry, there's no device with this IMEI currently in our system"
-          flash[:imei] = params[:imei]
+          device = Device.new
+          device.name = params[:name]
+          device.provision_status_id = 1
+          device.account_id = session[:account_id]
+          device.save
+          flash[:message] = 'The device "' + params[:name] + '" was created successfully'
+          redirect_to :controller => 'devices'
         end
       end
+    end
+  end
+  
+  # User can edit their device
+  def edit
+    if request.post?
+      device = Device.find(params[:id])
+      device.name = params[:name]
+      device.imei = params[:imei]
+      device.save
+      flash[:message] = 'Properties for "' + params[:name] + '" were updated successfully'
+      redirect_to :controller => "devices"
+    else
+      @device = Device.find(params[:id])  
     end
   end
   
