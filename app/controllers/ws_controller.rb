cbee661802e49d7531690bd36dbda1658ed2c770
(no author) <(no author)@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2007-07-02 19:09:12 +0000

    Simple HTTP service to save readings to database.

---
+++ app/controllers/ws_controller.rb
@@ -0,0 +1,32 @@
+# Controller that receives readings from HTTP devices
+# Verifies that the device currently exists and associates data
+# with appropriate device and account
+class WsController < ApplicationController
+
+  # Supports url params imei, lat, lng, spd, dir alt
+  def index
+    # Check that the device exists
+    device = Device.find_by_imei(params[:imei])
+    
+    if device
+      # TODO data check/validation
+      reading = Reading.new
+      reading.latitude = params[:lat]
+      reading.longitude = params[:lng]
+      reading.altitude = params[:alt]
+      reading.speed = params[:spd]
+      reading.direction = params[:dir]
+      reading.device_id = device.id
+      
+      # Save the reading
+      if reading.save
+        render_text "Success"
+      else
+        render_text "Error saving data"
+      end
+    # Error finding device
+    else
+       render_text "We're sorry, this device does not exist"
+    end
+  end
+end
