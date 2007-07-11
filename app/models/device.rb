f96227968d064f9320aa03d6b5deef1bbe0c8f67
salad <salad@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2007-07-11 17:45:52 +0000

    Only grab most recent reading.

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,7 +1,7 @@
 class Device < ActiveRecord::Base
   belongs_to :account
   validates_uniqueness_of :imei
-  has_many :readings, :order => "created_at desc", :limit => 10
+  has_many :readings, :order => "created_at desc", :limit => 1 # Gets the most recent reading
   has_many :geofences, :order => "created_at desc", :limit => 3
   
   # For now the provision_status_id is represented by
