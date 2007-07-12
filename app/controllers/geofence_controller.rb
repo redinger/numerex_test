c58d0d1dc5ccc17c159b48104c0d6bc980e700b6
(no author) <(no author)@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2007-07-12 21:02:41 +0000

    Basic integration with middleware WS.

--- app/controllers/geofence_controller.rb
+++ app/controllers/geofence_controller.rb
@@ -16,12 +16,25 @@ class GeofenceController < ApplicationController
         geofence.device_id = device.id
         geofence.save
         flash[:message] = 'Geofence created succesfully'
+        
+        # Get the bounds parameters
+        fence = params[:bounds].split(",")
+        lat = fence[0]
+        lng = fence[1]
+        rad = fence[2].to_f * 1609.344 # 1 mile in meters
+
+        # Call the middleware WS to set the geofence
+        url = 'http://localhost:8080/webservice/simple/at.ws?cmd=AT%24GEOFNC=1,' + rad.to_s + ',' + lat + ',' + lng + '&device=' + device.imei
+        resp = Net::HTTP.get_response(URI.parse(url))
+        # Could be a sync problem, testing for now
+        url = 'http://localhost:8080/webservice/simple/at.ws?cmd=AT%26w&device=' + device.imei
+        resp = Net::HTTP.get_response(URI.parse(url))
       else
         flash[:message] = 'You can have a total of three geofences per device'
       end
         redirect_to :controller => 'geofence', :action => 'view', :id => device.id
     else
-      @device = Device.get_device(params[:id], session[:account_id])  
+      @device = Device.get_device(params[:id], session[:account_id])
     end
   end
   
