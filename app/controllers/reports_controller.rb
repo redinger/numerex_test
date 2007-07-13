2f7c104855478c3bc2b7a3e1740e95eb76655e8e
(no author) <(no author)@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2007-07-13 18:55:35 +0000

    Basic reporting stuff.

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,2 +1,22 @@
 class ReportsController < ApplicationController
+  def index
+  end
+  
+  # Find all readings in descending order, limit 50
+  def all_readings
+    @readings = Reading.find(:all, :order => "created_at desc", :limit => 50)
+  end
+  
+  def stop
+    @readings = Reading.find(:all, :order => "created_at desc", :limit => 50, :conditions => "event_type='stop'")
+  end
+  
+  def start
+    @readings = Reading.find(:all, :order => "created_at desc", :limit => 50, :conditions => "event_type='start'")
+  end
+  
+  def speed
+    @readings = Reading.find(:all, :order => "created_at desc", :limit => 50, :conditions => "event_type='speed'")
+  end
+
 end
