b317134dcf5cded1a5de86db5e6a8cd5209b7f61
(no author) <(no author)@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2007-07-13 18:56:15 +0000

    Geofence unit test.

---
+++ test/unit/geofence_test.rb
@@ -0,0 +1,10 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+class GeofenceTest < Test::Unit::TestCase
+  fixtures :geofences
+
+  # Replace this with your real tests.
+  def test_truth
+    assert true
+  end
+end
