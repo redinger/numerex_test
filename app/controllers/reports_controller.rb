bfe18c09bee2be1908e69ec0a5be4b0968d8deb0
bappelt <bappelt@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2007-07-16 18:17:45 +0000

    adding stop event

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -5,10 +5,26 @@ class ReportsController < ApplicationController
   # Find all readings in descending order, limit 50
   def all_readings
     @readings = Reading.find(:all, :order => "created_at desc", :limit => 50)
+    puts @readings.first.class
   end
   
   def stop
-    @readings = Reading.find(:all, :order => "created_at desc", :limit => 50, :conditions => "event_type='stop'")
+    
+    
+    readings = Reading.find(:all, :order => "created_at asc", :limit => 50, :conditions => "event_type='stop_et41'")
+    @stops = Array.new
+    
+    readings.each_index { |index|
+    
+                            if readings[index].speed==0
+                            #  stopEvent = readings[index]
+                              stopEvent = {latitude=>99, durations=>9}
+                              #stopEvent.durations = readings[index+1].created_at - readings[index].created_at
+                              @stops.push stopEvent
+                            end
+                            
+                        }
+    
   end
   
   def start
