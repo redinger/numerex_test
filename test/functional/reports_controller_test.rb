e41413587acaf3f956887b76bd74ef34ca4e2190
bappelt <bappelt@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2007-07-18 19:51:19 +0000

    added extension to stop method not found errors, this is probably a hack
    added test for vehicle currently stopped

--- test/functional/reports_controller_test.rb
+++ test/functional/reports_controller_test.rb
@@ -6,12 +6,19 @@ class ReportsController; def rescue_action(e) raise e end; end
 
 class ReportsControllerTest < Test::Unit::TestCase
   
+  module PathInfo
+    def path_info
+      "asdf"
+    end
+  end
+  
   fixtures :users, :readings
   
   def setup
     @controller = ReportsController.new
-    @request    = ActionController::TestRequest.new
+    @request    = ActionController::TestRequest.new 
     @response   = ActionController::TestResponse.new
+    @request.extend(PathInfo)
   end
 
   # Replace this with your real tests.
@@ -20,15 +27,16 @@ class ReportsControllerTest < Test::Unit::TestCase
   end
   
   def test_stop
-    get :stop, {}, { :user => users(:dennis) } 
+    get :stop, {:id=>"3"}, { :user => users(:dennis) } 
     assert_response :success
     assert_template "stop"
     stops = assigns(:stops)
-    assert_equal 4, stops.size
+    assert_equal 5, stops.size
     assert_equal 600, stops[0].duration
     assert_equal 100, stops[1].duration
     assert_equal nil, stops[2].duration
     assert_equal 200, stops[3].duration
+    assert_equal nil, stops[4].duration
     
   end
 end
