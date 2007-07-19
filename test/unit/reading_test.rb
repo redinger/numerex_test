5769cec62b860cb12541b86f9bdf6a86189bbedf
bappelt <bappelt@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2007-07-19 22:47:01 +0000

    added tests for reverse geocoding

--- test/unit/reading_test.rb
+++ test/unit/reading_test.rb
@@ -7,4 +7,16 @@ class ReadingTest < Test::Unit::TestCase
   def test_truth
     assert true
   end
-end
+  
+  def test_address
+    assert_equal "6760 Big Springs Dr, Arlington, Texas", readings(:reading1).address
+    assert_equal "Inwood Rd, Farmers Branch, Texas", readings(:reading2).address
+    
+    Reading::ReverseGeocodeURL[8] = "A"  #muck up URL to create exception
+    assert_equal "32.6358, -97.1757", readings(:reading1).address
+  end
+  
+   
+  
+
+  end
