b821f4f74c8f8ccfaca4f736ef3e84c10255fc94
bappelt <bappelt@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2007-07-19 22:45:31 +0000

    added address method

--- app/models/reading.rb
+++ app/models/reading.rb
@@ -1,3 +1,27 @@
+require 'net/http'
+require "rexml/document"
+
 class Reading < ActiveRecord::Base
   belongs_to :device
+  
+  ReverseGeocodeURL = "http://ws.geonames.org/findNearestAddress"
+  def address
+      base = ReverseGeocodeURL + "?lat=" + latitude.to_s + "&lng=" + longitude.to_s
+   begin
+      resp = Net::HTTP.get_response(URI.parse(base))
+      data = resp.body
+      doc = REXML::Document.new data
+      streetNumber = doc.get_text('/geonames/address/streetNumber').to_s
+      restOfAddress = doc.get_text('/geonames/address/street').to_s + ", " + doc.get_text('/geonames/address/placename').to_s + ", " + doc.get_text('/geonames/address/adminName1').to_s
+    
+      if streetNumber==""
+        restOfAddress
+      else
+        streetNumber + " " + restOfAddress
+      end
+  rescue
+      latitude.to_s + ", " + longitude.to_s
+  end
+   
+  end
 end
