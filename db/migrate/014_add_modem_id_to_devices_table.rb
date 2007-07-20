2bb054d5f6c045e9f68ce6aa06360016134bc0f2
kowalski <kowalski@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2007-07-20 20:28:07 +0000

    OK new migration script 014

---
+++ db/migrate/014_add_modem_id_to_devices_table.rb
@@ -0,0 +1,9 @@
+class AddModemIdToDevicesTable < ActiveRecord::Migration
+ def self.up
+    add_column :devices, :modem_id, :string, :limit => 30
+  end
+
+  def self.down
+    remove_column :devices, :modem_id
+  end
+end
