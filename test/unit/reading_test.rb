df02b48d537f66ca3865adf5b646ff4284148c8a
bappelt <bappelt@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2007-07-23 02:30:11 +0000

    parse address XML from db

--- test/unit/reading_test.rb
+++ test/unit/reading_test.rb
@@ -8,12 +8,12 @@ class ReadingTest < Test::Unit::TestCase
     assert true
   end
   
+  
   def test_address
-    assert_equal "6760 Big Springs Dr, Arlington, Texas", readings(:reading1).address
-    assert_equal "Inwood Rd, Farmers Branch, Texas", readings(:reading2).address
-    
-    Reading::ReverseGeocodeURL[8] = "A"  #muck up URL to create exception
-    assert_equal "32.6358, -97.1757", readings(:reading1).address
+    assert_equal "6762 Big Springs Dr, Arlington, Texas", readings(:reading1).shortAddress
+    assert_equal "Inwood Rd, Farmers Branch, Texas", readings(:reading2).shortAddress
+    assert_equal "32.9395, -96.8244", readings(:reading3).shortAddress
+    assert_equal "32.94, -96.8217", readings(:reading4).shortAddress
   end
   
    
