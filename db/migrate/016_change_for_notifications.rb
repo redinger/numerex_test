702edb8fa74b8ddb318efa619ffd9359c59ca353
kowalski <kowalski@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2007-07-24 21:30:05 +0000

    New Migration for Notification stuff fix 016

--- db/migrate/016_change_for_notifications.rb
+++ db/migrate/016_change_for_notifications.rb
@@ -1,18 +1,12 @@
 class ChangeForNotifications < ActiveRecord::Migration
   def self.up
     add_column :readings, :notified, :string
-  end
-
-  def self.down
-    remove_column :readings, :notified
-  end
-
-  def self.up
     add_column :users, :notify, :integer, :default => 0
     add_column :users, :notifycount, :integer, :default => 20
   end
 
   def self.down
+    remove_column :readings, :notified
     remove_column :users, :notify
     remove_column :users, :notifycount
   end
