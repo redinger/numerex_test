4aa572e2996bd9a36c219c00e63531ddf74ebac1
salad <salad@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2007-07-24 04:06:28 +0000

    Basic settings work.

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,2 +1,11 @@
 class SettingsController < ApplicationController
+  def index
+    @account = Account.find(session[:account_id])    
+    if request.post?
+      @account.company = params[:company]
+      @account.save
+      session[:company] = params[:company]
+      redirect_to :action => 'index'
+    end
+  end
 end
