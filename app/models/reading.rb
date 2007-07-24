2ed99c278f0cbcc111d6a530e7d507c94435ab2d
bappelt <bappelt@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2007-07-24 19:49:10 +0000

    latest version of REXML will not raise exception on nil Document

--- app/models/reading.rb
+++ app/models/reading.rb
@@ -5,19 +5,23 @@ class Reading < ActiveRecord::Base
   belongs_to :device
 
   def shortAddress
-    begin
-        doc = REXML::Document.new address
-        streetNumber = doc.get_text('/geonames/address/streetNumber').to_s
-        street = doc.get_text('/geonames/address/street').to_s
-        city = doc.get_text('/geonames/address/placename').to_s 
-        state = doc.get_text('/geonames/address/adminName1').to_s
-        streetAddress = [streetNumber, street]
-        streetAddress.delete("")
-        shortAddress = [ streetAddress.join(' '), city, state]
-        shortAddress.delete("")
-        shortAddress.join(', ')
-    rescue
+    if(address.nil?)
       latitude.to_s + ", " + longitude.to_s
+    else
+      begin
+          doc = REXML::Document.new address
+          streetNumber = doc.get_text('/geonames/address/streetNumber').to_s
+          street = doc.get_text('/geonames/address/street').to_s
+          city = doc.get_text('/geonames/address/placename').to_s 
+          state = doc.get_text('/geonames/address/adminName1').to_s
+          streetAddress = [streetNumber, street]
+          streetAddress.delete("")
+          shortAddress = [ streetAddress.join(' '), city, state]
+          shortAddress.delete("")
+          shortAddress.join(', ')
+      rescue
+        latitude.to_s + ", " + longitude.to_s
+      end
     end
   end
   
