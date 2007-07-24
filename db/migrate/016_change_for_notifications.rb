cc102e5eebdd35b6c268b169cce5e0e923f47132
(no author) <(no author)@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2007-07-24 21:08:34 +0000

    Ken's the MASTER and said DO THIS because otherwise I'll punch him.  -- Change for User Notifications

--- db/migrate/016_change_for_notifications.rb
+++ db/migrate/016_change_for_notifications.rb
@@ -1,4 +1,4 @@
-class AddFlagToReadings < ActiveRecord::Migration
+class ChangeForNotifications < ActiveRecord::Migration
   def self.up
     add_column :readings, :notified, :string
   end
@@ -6,12 +6,10 @@ class AddFlagToReadings < ActiveRecord::Migration
   def self.down
     remove_column :readings, :notified
   end
-end
 
-class AddNotifyFlagToUsers < ActiveRecord::Migration
   def self.up
-    add_column :users, :notify, :integer, :default='0'
-    add_column :users, :notifycount, :integer, :default='20'
+    add_column :users, :notify, :integer, :default => 0
+    add_column :users, :notifycount, :integer, :default => 20
   end
 
   def self.down
