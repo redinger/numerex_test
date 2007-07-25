7f4322a43d9e068924fb6c4182f085f52c3caf2a
bappelt <bappelt@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2007-07-25 17:36:54 +0000

    added tests

--- test/functional/geofence_controller_test.rb
+++ test/functional/geofence_controller_test.rb
@@ -5,14 +5,40 @@ require 'geofence_controller'
 class GeofenceController; def rescue_action(e) raise e end; end
 
 class GeofenceControllerTest < Test::Unit::TestCase
+  
+  fixtures :devices, :users, :accounts, :geofences
+  
   def setup
     @controller = GeofenceController.new
     @request    = ActionController::TestRequest.new
     @response   = ActionController::TestResponse.new
   end
+  
+  def test_create
+    #apparent bug in rail makes flash go away after first post in this test
+    post :add, {:id => "1", :name => "qwerty", :bounds=>"1,1,1", :address=>"1600 Penn Ave"}, { :user => users(:dennis), :account_id => "1" }
+    assert_redirected_to :controller => "geofence", :action => "view" 
+    assert_flash_equal "Geofence created succesfully", :message
+    
+    post :add, {:id => "1", :name => "qwerty", :bounds=>"1,1,1", :address=>"1600 Penn Ave"}, { :user => users(:dennis), :account_id => "1" }
+    assert_redirected_to :controller => "geofence", :action => "view" 
 
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+    post :add, {:id => "1", :name => "qwerty", :bounds=>"1,1,1", :address=>"1600 Penn Ave"}, { :user => users(:dennis), :account_id => "1" }
+    assert_redirected_to :controller => "geofence", :action => "view" 
+    
+     post :add, {:id => "1", :name => "qwerty", :bounds=>"1,1,1", :address=>"1600 Penn Ave"}, { :user => users(:dennis), :account_id => "1" }
+    assert_redirected_to :controller => "geofence", :action => "view" 
+    
+     post :add, {:id => "1", :name => "qwerty", :bounds=>"1,1,1", :address=>"1600 Penn Ave"}, { :user => users(:dennis), :account_id => "1" }
+    assert_redirected_to :controller => "geofence", :action => "view" 
   end
+  
+  def test_delete
+    post :delete, {:geofence_id => '1', :device_id => '1'}, { :user => users(:dennis), :account_id => "1" }
+  end
+  
+  def test_edit
+     post :edit, {:device_id => '1', :geofence_id => '1', :name => "qwerty", :bounds=>"1,1,1", :address=>"1600 Penn Ave"}, { :user => users(:dennis), :account_id => "1" }
+  end
+  
 end
