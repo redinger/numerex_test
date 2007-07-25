299b627fbfba70b0aa20f61c77bc6c4ef659466d
bappelt <bappelt@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2007-07-25 17:34:59 +0000

    added find_fence_num

--- app/models/geofence.rb
+++ app/models/geofence.rb
@@ -1,3 +1,21 @@
 class Geofence < ActiveRecord::Base
   belongs_to :device
+  validates_uniqueness_of :fence_num, :scope => :device_id 
+  validates_presence_of :device_id, :fence_num
+  
+  def find_fence_num
+    for i in 1..4
+      if(device.get_fence_by_num(i).nil?)
+        num = i
+        break
+      end
+    end
+    if(num.nil?)
+      raise "no fences available"
+    else 
+      self.fence_num = num
+    end
+    
+  end
+    
 end
