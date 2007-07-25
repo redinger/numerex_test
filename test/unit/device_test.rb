dfbcf6d0113c99064db19da189d614e6160e3566
bappelt <bappelt@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2007-07-25 17:38:26 +0000

    added tests

--- test/unit/device_test.rb
+++ test/unit/device_test.rb
@@ -1,10 +1,27 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class DeviceTest < Test::Unit::TestCase
-  fixtures :devices
+  fixtures :devices, :geofences
 
   # Replace this with your real tests.
   def test_truth
     assert true
+  end
+  
+  def test_get_fence
+    fence = devices(:device1).get_fence_by_num(1)
+    assert_equal geofences(:fenceOne), fence
+    
+     fence = devices(:device1).get_fence_by_num(2)
+    assert_nil fence
+    
+    fence = devices(:device2).get_fence_by_num(2)
+    assert_nil fence
+  end
+  
+  def test_online
+    assert_equal true, devices(:device1).online?
+    assert_equal false, devices(:device2).online?
+    assert_equal false, devices(:device3).online?
   end
 end
--- test/unit/geofence_test.rb
+++ test/unit/geofence_test.rb
@@ -1,10 +1,56 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class GeofenceTest < Test::Unit::TestCase
-  fixtures :geofences
-
+  fixtures :geofences, :devices
+  
   # Replace this with your real tests.
   def test_truth
     assert true
+  end
+  
+  
+  def test_unique
+    fence1 = Geofence.new 
+    fence1.device_id = 1
+    fence1.fence_num = 1
+    fence1.save
+    
+    fence2 = Geofence.new 
+    fence2.device_id = 2
+    fence2.fence_num = 1
+    fence2.save!
+    
+    begin
+      fence3 = Geofence.new 
+      fence3.fence_num = 1
+      fence3.device_id = 2
+      fence3.save!
+      fail "should have thrown exception"
+    rescue
+      puts $!
+    end
+  end
+  
+  def test_find_fence_number
+    fence = Geofence.new
+    fence.device_id = 1
+    fence.find_fence_num
+    assert_equal 2, fence.fence_num
+    fence.save
+    
+    fence2 = Geofence.new
+    fence2.device_id = 1
+    fence2.find_fence_num
+    assert_equal 4, fence2.fence_num
+    fence2.save
+    
+    fence3 = Geofence.new
+    fence2.device_id = 1
+    begin
+      fence2.find_fence_num
+      fail "should have thrown exception"
+    rescue
+      puts $!
+    end
   end
 end
