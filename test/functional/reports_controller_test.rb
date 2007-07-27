6acb46f89d6357fb863810b4a7a5c40e67736bae
bappelt <bappelt@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2007-07-27 22:22:31 +0000

    fixed test

--- test/functional/reports_controller_test.rb
+++ test/functional/reports_controller_test.rb
@@ -27,16 +27,22 @@ class ReportsControllerTest < Test::Unit::TestCase
   end
   
   def test_stop
-    get :stop, {:id=>"3"}, { :user => users(:dennis) } 
-    assert_response :success
-    assert_template "stop"
-    stops = assigns(:stops)
-    assert_equal 5, stops.size
-    assert_equal 900, stops[0].duration
-    assert_equal 400, stops[1].duration
-    assert_equal nil, stops[2].duration
-    assert_equal 500, stops[3].duration
-    assert_equal nil, stops[4].duration
     
+    pretend_now_is(Time.at(1185490000)) do
+      get :stop, {:id=>"3", :t=>"1"}, { :user => users(:dennis) } 
+      stops = assigns(:stops)
+      assert_response :success
+      assert_template "stop"
+      assert_equal 3, stops.size
+      assert_equal 900, stops[0].duration
+      assert_equal 400, stops[1].duration
+      assert_equal nil, stops[2].duration
+      
+      get :stop, {:id=>"3", :t=>"1", :p => "2"}, { :user => users(:dennis) }
+      stops = assigns(:stops)
+      assert_equal 2, stops.size
+      assert_equal 500, stops[0].duration
+      assert_equal nil, stops[1].duration
+    end
   end
 end
