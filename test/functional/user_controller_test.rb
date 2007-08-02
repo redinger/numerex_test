bfca4daffede8de94397f8d65bbaaffa6294327f
bappelt <bappelt@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2007-08-02 22:29:02 +0000

    added tests

--- test/functional/user_controller_test.rb
+++ test/functional/user_controller_test.rb
@@ -5,14 +5,30 @@ require 'users_controller'
 class UserController; def rescue_action(e) raise e end; end
 
 class UserControllerTest < Test::Unit::TestCase
+  
+  fixtures :users,:accounts
+  
+    module RequestExtensions
+    def server_name
+      "helo"
+    end
+    def path_info
+      "adsf"
+    end
+  end
+  
   def setup
-    @controller = UserController.new
+    @controller = UsersController.new
     @request    = ActionController::TestRequest.new
     @response   = ActionController::TestResponse.new
+    @request.extend(RequestExtensions)
   end
 
   # Replace this with your real tests.
-  def test_truth
-    assert true
+  def test_edit
+    params = {:first_name => "qwerty", :last_name => "asdf", :email =>"asdf"}
+    post :edit, {:id => "1", :user => params}, { :user => users(:dennis), :account_id => "1" }
+    assert_redirected_to :controller => "users"
+    assert_equal "qwerty", User.find(1).first_name
   end
 end
