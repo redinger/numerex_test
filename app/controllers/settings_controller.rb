37372a3e3a2013a1eb0f4730019b242a91573286
(no author) <(no author)@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2007-08-02 21:25:15 +0000

    Settings controller to support notifications and timezones.

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,10 +1,14 @@
 class SettingsController < ApplicationController
   def index
-    @account = Account.find(session[:account_id])    
+    @account = Account.find(session[:account_id])   
+    @user = User.find(session[:user_id])
     if request.post?
-      @account.company = params[:company]
-      @account.save
+      @account.update_attribute('company', params[:company])
+      params[:notify] ? params[:notify] = 1 : params[:notify] = 0
+      @user.update_attributes!(:time_zone => params[:time_zone], :enotify => params[:notify])
+      
       session[:company] = params[:company]
+      flash[:message] = 'Settings saved successfully'
       redirect_to :action => 'index'
     end
   end
--- test/functional/settings_controller_test.rb
+++ test/functional/settings_controller_test.rb
@@ -5,14 +5,44 @@ require 'settings_controller'
 class SettingsController; def rescue_action(e) raise e end; end
 
 class SettingsControllerTest < Test::Unit::TestCase
+  
+  fixtures :accounts, :users
+  
+  module RequestExtensions
+    def server_name
+      "yoohoodilly"
+    end
+    def path_info
+      "asdf"
+    end
+  end
+  
   def setup
     @controller = SettingsController.new
     @request    = ActionController::TestRequest.new
     @response   = ActionController::TestResponse.new
+    @request.extend(RequestExtensions)
   end
 
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  def test_index
+    # Get the settings page
+    get :index, {}, {:account_id => 1, :user_id => 1}
+    assert_response :success
+    account = accounts(:dennis)
+    user = users(:dennis)
+    assert_equal 'Dennis Co', account.company
+    assert_equal false, user.enotify
+    assert_equal nil, user.time_zone
+    
+    # Post the settings
+    post :index, {:company => 'New Co', :notify => 1, :time_zone => 'Central Time (US & Canada)'}, {:account_id => 1, :user_id => 1}
+    assert_redirected_to :controller => 'settings', :action => 'index'
+    
+    # Verify the settings were saved
+    account = assigns(:account)
+    user = assigns(:user)
+    assert_equal 'New Co', account.company
+    assert_equal true, user.enotify
+    assert_equal 'Central Time (US & Canada)', user.time_zone
   end
 end
