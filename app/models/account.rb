df400acb45a33d27e6bf6eb0f2b7e51a6999d65c
bappelt <bappelt@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2007-08-22 19:52:23 +0000

    added constraints

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,4 +1,8 @@
 class Account < ActiveRecord::Base
   has_many :devices
   has_many :users
+  validates_uniqueness_of :subdomain, :case_sensitive => false
+  validates_presence_of :zip, :message => "Please specify your zip code"
+  validates_presence_of :company, :message => "Please specify your company or group name"
+  validates_presence_of :subdomain, :message => "Please specify a subdomain"
 end
