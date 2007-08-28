2f7cc6e2abdee57e80c1349301f4006aa12fd12f
dbaldwin <dbaldwin@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2007-08-28 14:26:54 +0000

    Setting focus to first name field when form loads.

--- public/javascripts/order.js
+++ public/javascripts/order.js
@@ -1,6 +1,7 @@
 var states = ['Alabama','Alaska','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','Florida','Georgia','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Ohio','Oklahoma','Oregon','Pennsylvania','Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virginia','Washington','Washington DC','West Virginia','Wisconsin','Wyoming'];
 
 function initStep1() {
+	document.getElementById('ship_first_name').focus();
 	populateStates('ship_state');
 }
 
