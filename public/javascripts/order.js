8d7899c4b8349b4db74e0ac6e28153ab32d0f7ab
dbaldwin <dbaldwin@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2007-08-29 15:55:24 +0000

    Javascript utils and validation.

--- public/javascripts/order.js
+++ public/javascripts/order.js
@@ -1,4 +1,4 @@
-var states = ['Alabama','Alaska','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','Florida','Georgia','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Ohio','Oklahoma','Oregon','Pennsylvania','Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virginia','Washington','Washington DC','West Virginia','Wisconsin','Wyoming'];
+var states = [{name:'Alabama',value:'AL'},{name:'Alaska',value:'AK'},{name:'Arizona',value:'AZ'},{name:'Arkansas',value:'AR'},{name:'California',value:'CA'},{name:'Colorado',value:'CO'},{name:'Connecticut',value:'CT'},{name:'Delaware',value:'DE'},{name:'Florida',value:'FL'},{name:'Georgia',value:'GA'},{name:'Hawaii',value:'HI'},{name:'Idaho',value:'ID'},{name:'Illinois',value:'IL'},{name:'Indiana',value:'IN'},{name:'Iowa',value:'IA'},{name:'Kansas',value:'KS'},{name:'Kentucky',value:'KY'},{name:'Louisiana',value:'LA'},{name:'Maine',value:'ME'},{name:'Maryland',value:'MD'},{name:'Massachusetts',value:'MA'},{name:'Michigan',value:'MI'},{name:'Minnesota',value:'MN'},{name:'Mississippi',value:'MS'},{name:'Missouri',value:'MO'},{name:'Montana',value:'MT'},{name:'Nebraska',value:'NE'},{name:'Nevada',value:'NV'},{name:'New Hampshire',value:'NH'},{name:'New Jersey',value:'NJ'},{name:'New Mexico',value:'NM'},{name:'New York',value:'NY'},{name:'North Carolina',value:'NC'},{name:'North Dakota',value:'ND'},{name:'Ohio',value:'OH'},{name:'Oklahoma',value:'OK'},{name:'Oregon',value:'OR'},{name:'Pennsylvania',value:'PA'},{name:'Rhode Island',value:'RI'},{name:'South Carolina',value:'SC'},{name:'South Dakota',value:'SD'},{name:'Tennessee',value:'TN'},{name:'Texas',value:'TX'},{name:'Utah',value:'UT'},{name:'Vermont',value:'VT'},{name:'Virginia',value:'VA'},{name:'Washington',value:'WA'},{name:'Washington DC',value:'DC'},{name:'West Virginia',value:'WV'},{name:'Wisconsin',value:'WI'},{name:'Wyoming',value:'WY'}];
 
 function initStep1() {
 	document.getElementById('ship_first_name').focus();
@@ -24,11 +24,10 @@ function populateStates(id) {
 	option.value = '';
 	e.appendChild(option);
 
-	
 	for(var i=0; i < states.length; i++) {
 		var option = document.createElement('option');
-		option.innerHTML = states[i]
-		option.value = '';
+		option.innerHTML = states[i].name
+		option.value = states[i].value;
 		e.appendChild(option);
 	}
 }
@@ -51,3 +50,49 @@ function toggleOrderButton(checked) {
 		order_btn.disabled = true;
 }
 
+function validateShipForm(form) {
+	resetFields(form);
+	
+	var ship_first_name = form.ship_first_name.value.trim();
+	if(ship_first_name.length < 1) {
+		alert('Please enter a valid shipping first name');
+		form.ship_first_name.className = 'error_short';
+		form.ship_first_name.focus();
+		return false;
+	}
+	
+	var ship_last_name = form.ship_last_name.value.trim();
+	if(ship_last_name.length < 1) {
+		alert('Please enter a valid shipping last name');		
+		form.ship_last_name.className = 'error_short';
+		form.ship_last_name.focus();
+		return false;
+	}
+	
+	var ship_address = form.ship_address.value.trim();
+	if(ship_address.length < 4) {
+		alert('Please enter a valid shipping address');		
+		form.ship_address.className = 'error_long';
+		form.ship_address.focus();
+		return false;
+	}
+	
+	var ship_city = form.ship_city.value.trim();
+	if(ship_city.length < 2) {
+		alert('Please enter a valid shipping city');		
+		form.ship_city.className = 'error_short';
+		form.ship_city.focus();
+		return false;
+	}
+	
+	var ship_state = form.ship_state
+	
+	return true;
+}
+
+function resetFields(form) {
+	form.ship_first_name.className = 'short_text';
+	form.ship_last_name.className = 'short_text';
+	form.ship_address.className = 'long_text';
+	form.ship_city.className = 'short_text';
+}
--- public/javascripts/utils.js
+++ public/javascripts/utils.js
@@ -1,3 +1,14 @@
+// String trim functions
+String.prototype.trim = function() {
+	return this.replace(/^\s+|\s+$/g,"");
+}
+String.prototype.ltrim = function() {
+	return this.replace(/^\s+/,"");
+}
+String.prototype.rtrim = function() {
+	return this.replace(/\s+$/,"");
+}
+
 // Function to display localized date/time
 function displayLocalDT(dt) {
 	return (new Date(dt).toString().split(" GMT")[0]);
