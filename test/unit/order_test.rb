e6c1ba78a869ae6ea8ff5f2c71a212d8bcb59272
dbaldwin <dbaldwin@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2007-09-05 20:27:12 +0000

    Order unit test.

--- test/unit/order_test.rb
+++ test/unit/order_test.rb
@@ -1,10 +1,100 @@
 require File.dirname(__FILE__) + '/../test_helper'
+require File.dirname(__FILE__) + '/../../vendor/plugins/paypal/lib/caller'
+require File.dirname(__FILE__) + '/../../vendor/plugins/paypal/lib/profile'
 
 class OrderTest < Test::Unit::TestCase
-  fixtures :orders
-
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  def setup
+      @caller = PayPalSDKCallers::Caller.new(false)
+      @shipping = 8.95
   end
+  def test_do_direct_payment_monthly
+     req= {
+          :method          => 'DoDirectPayment',
+          :amt             => '295.70',
+          :currencycode    => 'USD',
+          :paymentaction   => 'authorization',
+          :creditcardtype  => 'Visa',
+          :acct            => '4721930402892796',
+          :firstname       => 'Dennis',
+          :lastname        => 'Baldwin',
+          :email           => 'dennis@ublip.com',
+          :street          => '6246 Ellsworth Avenue',
+          :city            => 'Dallas',
+          :state           => 'TX',
+          :zip             => '75214',
+          :countrycode     => 'US',
+          :expdate         => '122010',
+          :cvv2            => '396',
+          :shiptoname      => 'Dennis Baldwin',
+          :shiptostreet    => '6246 Ellsworth Avenue',
+          :shiptostreet2   => '',
+          :shiptocity      => 'Dallas',
+          :shiptostate     => 'TX',
+          :shiptocountrycode => 'US',
+          :shiptophonenum  => '2147383404',
+          :shiptozip       => '75214',
+          :itemamt         => '264.90',
+          :shippingamt     => @shipping,
+          :taxamt          => '21.85',  
+          :l_name0         => 'Ublip Tracking Device',
+          :l_number0       => 'UD1000',
+          :l_qty0          => '1',
+          :l_amt0          => '249.95',
+          :l_name1         => 'Ublip Monthly Tracking Service',
+          :l_number1       => 'US1000',
+          :l_qty1          => '1',
+          :l_amt1          => '14.95'  
+      }
+      @transaction = @caller.call(req)
+      puts '---------------'
+      puts @transaction.response
+      puts '---------------'
+      assert_equal 'Success', @transaction.response["ACK"].to_s       
+ end
+
+  def test_do_direct_payment_yearly
+      req= {
+            :method          => 'DoDirectPayment',
+            :amt             => '441.80',
+            :currencycode    => 'USD',
+            :paymentaction   => 'authorization',
+            :creditcardtype  => 'MasterCard',
+            :acct            => '4721930402892796',
+            :firstname       => 'Dennis',
+            :lastname        => 'Baldwin',
+            :email           => 'dennis@ublip.com',
+            :street          => '6246 Ellsworth Avenue',
+            :city            => 'Dallas',
+            :state           => 'TX',
+            :zip             => '75214',
+            :countrycode     => 'US',
+            :expdate         => '122010',
+            :cvv2            => '396',
+            :shiptoname      => 'Dennis Baldwin',
+            :shiptostreet    => '6246 Ellsworth Avenue',
+            :shiptostreet2   => '',
+            :shiptocity      => 'Dallas',
+            :shiptostate     => 'TX',
+            :shiptocountrycode => 'US',
+            :shiptophonenum  => '2147383404',
+            :shiptozip       => '75214',
+            :itemamt         => '399.95',
+            :shippingamt     => @shipping,
+            :taxamt          => '32.90',
+            :l_name0         => 'Ublip Tracking Device',
+            :l_number0       => 'UD1000',
+            :l_qty0          => '1',
+            :l_amt0          => '249.95',
+            :l_name1         => 'Ublip Annual Tracking Service',
+            :l_number1       => 'US1001',
+            :l_qty1          => '1',
+            :l_amt1          => '150.00'  
+        }
+        @transaction = @caller.call(req)
+        puts '---------------'
+        puts @transaction.response
+        puts '---------------'
+        assert_equal 'Success', @transaction.response["ACK"].to_s 
+    end
+
 end
