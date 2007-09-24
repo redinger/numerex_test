20e5948f3b02f89d279022f63de5ce1935c07f7a
dbaldwin <dbaldwin@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2007-09-24 14:42:32 +0000

    Support for persisting user time zone.

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -5,9 +5,10 @@ class SettingsController < ApplicationController
     if request.post?
       @account.update_attribute('company', params[:company])
       params[:notify] ? params[:notify] = 1 : params[:notify] = 0
-      @user.update_attribute('enotify', params[:notify])
+      @user.update_attributes!(:time_zone => params[:time_zone], :enotify => params[:notify])
       
       session[:company] = params[:company]
+      session[:time_zone] = params[:time_zone]
       flash[:message] = 'Settings saved successfully'
       redirect_to :action => 'index'
     end
--- test/functional/settings_controller_test.rb
+++ test/functional/settings_controller_test.rb
@@ -31,17 +31,18 @@ class SettingsControllerTest < Test::Unit::TestCase
     account = accounts(:dennis)
     user = users(:dennis)
     assert_equal 'Dennis Co', account.company
+    assert_equal 'Central Time (US & Canada)', user.time_zone
     assert_equal true, user.enotify
-    assert_equal nil, user.time_zone
     
     # Post the settings
-    post :index, {:company => 'New Co', :notify => 1}, {:account_id => 1, :user_id => 1}
+    post :index, {:company => 'New Co', :notify => 1, :time_zone => 'Eastern Time (US & Canada)'}, {:account_id => 1, :user_id => 1}
     assert_redirected_to :controller => 'settings', :action => 'index'
     
     # Verify the settings were saved
     account = assigns(:account)
     user = assigns(:user)
     assert_equal 'New Co', account.company
+    assert_equal 'Eastern Time (US & Canada)', user.time_zone
     assert_equal true, user.enotify
   end
 end
