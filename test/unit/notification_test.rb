ba762d0d958c4f292ac8e27f0ee5c05be7f858d1
dbaldwin <dbaldwin@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2007-09-24 20:55:47 +0000

    Notification tests.

--- test/unit/notification_test.rb
+++ test/unit/notification_test.rb
@@ -1,10 +1,35 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class NotificationTest < Test::Unit::TestCase
-  fixtures :notifications
+  fixtures :notifications, :users, :readings, :devices
 
+  def setup
+    ActionMailer::Base.delivery_method = :test
+    ActionMailer::Base.perform_deliveries = true
+    ActionMailer::Base.deliveries = []
+  end
+  
   # Replace this with your real tests.
   def test_truth
     assert_equal "device_offline", notifications(:one).notification_type.to_s
+  end
+  
+  def test_notify_geofence_exception
+    user = users(:dennis)
+    reading = readings(:readings_049) # Geofence exception reading
+    # Action code from notifier daemon
+    action = reading.event_type.include?('exit') ? "exited geofence " : "entered geofence "
+    action += reading.get_fence_name unless reading.get_fence_name.nil?
+    response = Notifier.deliver_notify_reading(user, action, reading)
+    assert_equal 'device 1 exited geofence home', response.subject
+    assert_match /Dear #{user.first_name} #{user.last_name}/, response.body
+  end
+  
+  def test_notify_device_offline
+    user = users(:dennis)
+    device = devices(:device1)
+    response = Notifier.deliver_device_offline(user, device)
+    assert_equal 'Device Offline Notification', response.subject
+    assert_match /Dear #{user.first_name} #{user.last_name},\n\ndevice 1 seems to be offline/, response.body
   end
 end
