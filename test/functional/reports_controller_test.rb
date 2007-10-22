65145be3eb76727e1601f88e5b359e66eb8ccddb
bappelt <bappelt@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2007-10-22 16:10:26 +0000

    only show in progress for last stop event

--- test/functional/reports_controller_test.rb
+++ test/functional/reports_controller_test.rb
@@ -43,7 +43,7 @@ class ReportsControllerTest < Test::Unit::TestCase
       puts "now is:" + Time.now.to_s
       get :stop, {:id=>"3", :t=>"1"}, { :user => users(:dennis) } 
       stops = assigns(:stops)
-      assert_equal 7, assigns(:record_count)
+      assert_equal 8, assigns(:record_count)
       assert_response :success
       assert_template "stop"
       
@@ -52,28 +52,33 @@ class ReportsControllerTest < Test::Unit::TestCase
       assert_equal -1, stops[0].duration
       assert_equal Time.local(2007, "Jul", 26, 16, 55, 0), stops[0].created_at
       
-      assert_equal 3480, stops[1].duration
-      assert_equal Time.local(2007, "Jul", 26, 15, 0, 0), stops[1].created_at
+      assert_nil stops[1].duration
+      assert_equal Time.local(2007, "Jul", 26, 16, 00, 0), stops[1].created_at
       
-      assert_equal 780, stops[2].duration
-      assert_equal Time.local(2007, "Jul", 26, 14, 48, 39), stops[2].created_at
+      assert_equal 3480, stops[2].duration
+      assert_equal Time.local(2007, "Jul", 26, 15, 0, 0), stops[2].created_at
       
       assert_equal 780, stops[3].duration
-      assert_equal Time.local(2007, "Jul", 26, 14, 37, 39), stops[3].created_at
+      assert_equal Time.local(2007, "Jul", 26, 14, 48, 39), stops[3].created_at
       
-      assert_equal 480, stops[4].duration
-      assert_equal Time.local(2007, "Jul", 26, 14, 30, 59), stops[4].created_at
+      assert_equal 780, stops[4].duration
+      assert_equal Time.local(2007, "Jul", 26, 14, 37, 39), stops[4].created_at
+      
+     
       
       get :stop, {:id=>"3", :t=>"1", :p => "2"}, { :user => users(:dennis) }
       stops = assigns(:stops)
       
-      assert_equal 2, stops.size
+      assert_equal 3, stops.size
       
-      assert_equal 280, stops[0].duration
-      assert_equal Time.local(2007, "Jul", 26, 14, 27, 39), stops[0].created_at
+      assert_equal 480, stops[0].duration
+      assert_equal Time.local(2007, "Jul", 26, 14, 30, 59), stops[0].created_at
       
-      assert_equal 780, stops[1].duration
-      assert_equal Time.local(2007, "Jul", 26, 14, 15, 59), stops[1].created_at
+      assert_equal 280, stops[1].duration
+      assert_equal Time.local(2007, "Jul", 26, 14, 27, 39), stops[1].created_at
+      
+      assert_equal 780, stops[2].duration
+      assert_equal Time.local(2007, "Jul", 26, 14, 15, 59), stops[2].created_at
       
     end
   end
