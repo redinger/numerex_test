6b2495858c822a1406c941af1c306fd9b9cbd981
bappelt <bappelt@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2007-11-09 21:53:47 +0000

    added HTTP BASIC auth

--- app/controllers/readings_controller.rb
+++ app/controllers/readings_controller.rb
@@ -1,13 +1,17 @@
 class ReadingsController < ApplicationController
-  before_filter :authorize, :except => ['recent_public']
+ 
+  
+  before_filter :authorize_http, :except => ['recent_public']
   
   #Get most recent readings for all devices
   def recent
-    if(params[:id].nil?)
-      @devices = Device.get_devices(session[:account_id])
-    else
-      @devices = Device.find(:all, :conditions => ["id = ?", params[:id]])
-    end
+  #  if(params[:id].nil?)
+   #   @devices = Device.get_devices(session[:account_id])
+    #else
+     # @devices = Device.find(:all, :conditions => ["id = ?", params[:id]])
+     @device = Device.find( params[:id])
+     @locations = @device.readings
+    
     
     # Prevent the layout from getting in the way
     render :layout => false
@@ -15,14 +19,19 @@ class ReadingsController < ApplicationController
   
   # Display last N readings for given device
   def last
-    # TODO fix this so that the user can only view readings for their device
-    @readings = Reading.find(:all, :conditions => ["device_id = ?", params[:id]], :limit => 25, :order => "created_at desc")
+    account = Account.find(:first, :conditions => ["subdomain = ?", request.host.split('.')[0]])
+    device = Device.find(params[:id])
+    if(device.account_id == account.id)
+      @locations = Reading.find(:all, :conditions => ["device_id = ?", params[:id]], :limit => 1, :order => "created_at desc")
+    else
+      @locations = Array.new
+    end
     render :layout => false
   end
   
   # Simple test for iGoogle integration
   def recent_public
-    reading = Reading.find(:all, :conditions => ["device_id = ?", params[:id]], :limit => 1, :order => "created_at desc")
+    @readings = Reading.find(:all, :conditions => ["device_id = ?", params[:id]], :limit => 1, :order => "created_at desc")
     render_xml reading.to_xml
   end
 end
