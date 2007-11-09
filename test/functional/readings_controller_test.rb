e812dd298c3f87df85b2e645ad5d5e5a110b8e13
bappelt <bappelt@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2007-11-09 21:55:20 +0000

    added HTTP BASIC auth

--- test/functional/readings_controller_test.rb
+++ test/functional/readings_controller_test.rb
@@ -5,14 +5,40 @@ require 'readings_controller'
 class ReadingsController; def rescue_action(e) raise e end; end
 
 class ReadingsControllerTest < Test::Unit::TestCase
+  
+  fixtures :users,:accounts
+  
   def setup
     @controller = ReadingsController.new
     @request    = ActionController::TestRequest.new
     @response   = ActionController::TestResponse.new
   end
 
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  def test_last
+    @request.host="dennis.ublip.com"
+    @request.env["Authorization"] = "Basic " + Base64.encode64("dennis@ublip.com:testing")
+    get :last, { :id => "1"}, {:user => users(:dennis), :user_id => users(:dennis), :account_id => accounts(:dennis)}
+    puts @response.body
+    
+    assert_select "channel>item" do |element|
+       assert_tag :tag => "georss:point", :content => "32.6358 -97.1757"
+    end
   end
+  
+  def test_last_not_auth
+    @request.host="dennis.ublip.com"
+    @request.env["Authorization"] = "Basic " + Base64.encode64("dennis@ublip.com:testing")
+    get :last, { :id => "7"}, {:user => users(:dennis), :user_id => users(:dennis), :account_id => accounts(:dennis)}
+    puts @response.body
+    
+    assert_select "channel" do |element|
+    element[0].children.each do |tag|
+           if tag.class==HTML::Tag && tag.name=="item"
+             fail("should not return any content")
+           end
+    end
+      
+    end
+  end
+  
 end
