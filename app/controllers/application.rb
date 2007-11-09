94ff39b50131bc3ad12074f4ad6e35898d60818c
bappelt <bappelt@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2007-11-09 21:53:22 +0000

    added HTTP BASIC auth

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -28,4 +28,25 @@ class ApplicationController < ActionController::Base
       raise "not authorized"
     end
   end
+  
+  def authorize_http
+      username, passwd = get_auth_data
+      unless User.authenticate(request.subdomains.first, username, passwd) then access_denied end
+    end
+    
+    def access_denied
+          headers["Status"]           = "Unauthorized"
+          headers["WWW-Authenticate"] = %(Basic realm="Web Password")
+          render :text => "Could't authenticate you", :status => '401 Unauthorized'
+      false
+    end  
+  
+  private
+    @@http_auth_headers = %w(X-HTTP_AUTHORIZATION HTTP_AUTHORIZATION Authorization)
+    # gets BASIC auth info
+    def get_auth_data
+      auth_key  = @@http_auth_headers.detect { |h| request.env.has_key?(h) }
+      auth_data = request.env[auth_key].to_s.split unless auth_key.blank?
+      return auth_data && auth_data[0] == 'Basic' ? Base64.decode64(auth_data[1]).split(':')[0..1] : [nil, nil] 
+    end
 end
