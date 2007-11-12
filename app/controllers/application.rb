c71b46e1fac0af3cebb49d8aaa18252c18b089aa
bappelt <bappelt@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2007-11-12 20:49:49 +0000

    added filters for device and users

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -9,7 +9,6 @@ class ApplicationController < ActionController::Base
   
   private
   def authorize
-    puts request.parameters["action"]
     unless session[:user]
       flash[:message] = "You're not currently logged in"
       if(request.parameters["action"]=="map")
@@ -18,6 +17,20 @@ class ApplicationController < ActionController::Base
       else
         redirect_to :controller => "login"
       end
+    end
+  end
+  
+  def authorize_device
+    device = Device.find(params[:id])
+    unless device.account_id == session[:account_id]
+      redirect_back_or_default "/index"
+    end
+  end
+  
+  def authorize_user
+    user = User.find(params[:id])
+    unless user.account_id == session[:account_id]
+      redirect_back_or_default "/index"
     end
   end
   
@@ -40,6 +53,13 @@ class ApplicationController < ActionController::Base
           render :text => "Could't authenticate you", :status => '401 Unauthorized'
       false
     end  
+    
+    # Redirect to the URI stored by the most recent store_location call or
+    # to the passed default.
+    def redirect_back_or_default(default)
+      session[:return_to] ? redirect_to_url(session[:return_to]) : redirect_to(default)
+      session[:return_to] = nil
+    end
   
   private
     @@http_auth_headers = %w(X-HTTP_AUTHORIZATION HTTP_AUTHORIZATION Authorization)
