639ee5de0d90732e572c4b38a32b8c2badeabc80
bappelt <bappelt@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2007-11-12 20:53:11 +0000

    fixed meaningless test

--- test/functional/settings_controller_test.rb
+++ test/functional/settings_controller_test.rb
@@ -28,8 +28,8 @@ class SettingsControllerTest < Test::Unit::TestCase
     # Get the settings page
     get :index, {}, {:account_id => 1, :user_id => 1}
     assert_response :success
-    account = accounts(:dennis)
-    user = users(:dennis)
+    account = assigns(:account)
+    user = assigns(:user)
     assert_equal 'Dennis Co', account.company
     assert_equal 'Central Time (US & Canada)', user.time_zone
     assert_equal true, user.enotify
