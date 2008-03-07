b267960e5cf4ff0f7b31f5ca9657999981eddaf5
amills <amills@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2008-03-07 20:42:34 +0000

    Adding controller to check application status (including a quick DB query to verify DB functionality).

---
+++ test/functional/admin_status_controller_test.rb
@@ -0,0 +1,20 @@
+require File.dirname(__FILE__) + '/../test_helper'
+require 'admin_status_controller'
+
+# Re-raise errors caught by the controller.
+class AdminStatusController; def rescue_action(e) raise e end; end
+  
+class AdminStatusControllerTest < Test::Unit::TestCase
+  def setup
+    @controller = AdminStatusController.new
+    @request    = ActionController::TestRequest.new
+    @response   = ActionController::TestResponse.new
+  end
+  
+  def test_up_check
+    puts @request
+    get :up_check, {}, {}
+    assert_response(:success)
+    assert_match %r{Schema #: [0-9]+}, @response.body
+  end
+end
