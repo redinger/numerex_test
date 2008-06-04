8d8d7ea384bd4a7db2a044d8280f4d94f390becc
bappelt <bappelt@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2008-06-04 22:57:15 +0000

    load geofence fixture

--- test/unit/reading_test.rb
+++ test/unit/reading_test.rb
@@ -1,12 +1,7 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class ReadingTest < Test::Unit::TestCase
-  fixtures :readings
-
-  # Replace this with your real tests.
-  def test_truth
-    assert true
-  end
+  fixtures :readings, :geofences
   
   
   def test_address
