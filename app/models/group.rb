4b835a4938ac9b2e1d573aca8ff33f45ef2147c6
sdate <sdate@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2008-06-13 22:47:50 +0000

    ensure that only provisioned devices are shown in the groups

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,7 @@
 class Group < ActiveRecord::Base
  belongs_to :account
- has_many :devices
- include ApplicationHelper 
+ has_many :devices, :conditions => "provision_status_id = 1"
+ include ApplicationHelper
  
  validates_presence_of :name
 end
