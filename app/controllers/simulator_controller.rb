0df866c2a99e733538d961ae9c5d42e79b87f116
dbaldwin <dbaldwin@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2008-06-18 18:46:06 +0000

    Simple simulator to click on map, insert readings, and test geofences.

---
+++ app/controllers/simulator_controller.rb
@@ -0,0 +1,23 @@
+class SimulatorController < ApplicationController
+  before_filter :authorize
+
+  def index
+    @devices = Device.get_devices(session[:account_id])
+    @geofences = Geofence.find(:all, :conditions => ["account_id = ?", session[:account_id]])
+  end
+  
+  def insert_reading
+    reading = Reading.new
+    reading.device_id = params[:device_id]
+    reading.latitude = params[:lat]
+    reading.longitude = params[:lng]
+    reading.save
+    render_text "ok"
+  end
+  
+  def get_geofences_for_device
+    @geofences = Geofence.find(:all, :conditions => ["device_id = ?", params[:device_id]], :order => "name asc")
+    render :layout => false
+  end
+  
+end
