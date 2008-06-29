d14ff24025b21d12b756e62c5404832e14d6e70f
dbaldwin <dbaldwin@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2008-06-29 21:34:16 +0000

    Setting staging so hopefully notifier will pick up staging db config.

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,4 +1,5 @@
 # Settings specified here will take precedence over those in config/environment.rb
+ENV['RAILS_ENV'] ||= 'staging'
 
 # The production environment is meant for finished, "live" apps.
 # Code is not reloaded between requests
