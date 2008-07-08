797b7a6f2a1c219428053b0feb7e89c70c2a81dd
bappelt <bappelt@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2008-07-08 20:00:42 +0000

    pass rails env

--- config/database_procs.rb
+++ config/database_procs.rb
@@ -1,7 +1,7 @@
 task :setup_db_procs do
   sudo "mkdir -p /var/run/ublip_db"
   sudo "chown ublip:ublip /var/run/ublip_db"
-  run "cd #{current_path} && rake setup_db_scripts"
+  run "cd #{current_path} && rake setup_db_scripts RAILS_ENV=#{rails_env}"
   run "crontab -u #{user} -l | grep -v 'stopreport.sh' > oldcrontab"
   run "echo '* * * * * #{current_path}/stopreport.sh >> #{current_path}/log/stopreport.log 2>&1' >> oldcrontab"
   run "crontab -u #{user} oldcrontab"
