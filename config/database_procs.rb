036c4b9d44305dfcc40dd826b05e41ea09df7dc1
bappelt <bappelt@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2008-07-08 20:29:54 +0000

    set script to executable handle case where no crontab exists

--- config/database_procs.rb
+++ config/database_procs.rb
@@ -1,8 +1,18 @@
 task :setup_db_procs do
+  sudo "chmod a+x #{current_path}/stopreport.sh"
   sudo "mkdir -p /var/run/ublip_db"
   sudo "chown ublip:ublip /var/run/ublip_db"
   run "cd #{current_path} && rake setup_db_scripts RAILS_ENV=#{rails_env}"
-  sudo "crontab -u #{user} -l | grep -v 'stopreport.sh' > oldcrontab"
+
+  begin  
+    sudo "crontab -u #{user} -l" do |channel, stream, data|
+      if !data.include?('no crontab for') #only get current contents if there is a crontab
+        sudo "crontab -u #{user} -l | grep -v 'stopreport.sh' > oldcrontab"
+      end
+    end
+  rescue
+  end
+
   sudo "echo '* * * * * #{current_path}/stopreport.sh >> #{current_path}/log/stopreport.log 2>&1' >> oldcrontab"
   sudo "crontab -u #{user} oldcrontab"
   sudo "rm oldcrontab"
