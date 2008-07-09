9dc7039ebe0b1a21d091abfc4e1cddd0756181cf
sdate <sdate@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2008-07-09 22:54:44 +0000

    modified stop action to use Stop Events

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -30,7 +30,7 @@ class ReportsController < ApplicationController
     @device_names = Device.get_names(session[:account_id])
     @stop_events = StopEvent.find(:all, :conditions => ["device_id = ? and created_at between ? and ?", params[:id], @start_time, @end_time], :order => "created_at desc")
     @pages,@readings = paginate_collection(:collection => @stop_events,:page => params[:page],:per_page => ResultCount)   
-    @record_count = StopEvent.count('id', :conditions => ["device_id = ? and created_at between ? and ?", params[:id], @start_time, @end_time])
+    @record_count = StopEvent.count('id', :conditions => ["device_id = ? and date(created_at) between ? and ?", params[:id], @start_time, @end_time])
     @actual_record_count = @record_count # this is because currently we are putting  MAX_LIMIT on export data so export and view data going to be diferent in numbers.
     @record_count = MAX_LIMIT if @record_count > MAX_LIMIT
   end
