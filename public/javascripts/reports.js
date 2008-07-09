93fc05ae6b1b40cbf54f0ab2c8d759615e84e676
sdate <sdate@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2008-07-09 19:58:15 +0000

    minor tweaks to common.css and reports.js

--- public/javascripts/reports.js
+++ public/javascripts/reports.js
@@ -1,10 +1,15 @@
 // Switch between devices on reports view but keep the timeframe, if it exists
-function changeDevice(device_id, report_type) {
+function changeDevice(device_id, report_type, start_time, end_time) {
 	var url = '/reports/' + report_type + '/' + device_id;
 	
-	if(document.location.search != '')
+	if(document.location.search != ''){
 		url += '?t=' + getQryParam('t');
-		
+         }
+         else{
+                url += '?t=undefined';
+         } 
+	 url  =url+ "&end_time1=" + end_time + "&start_time1=" + start_time;	
+     
 	document.location.href= url;
 }
 
