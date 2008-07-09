903f8a1d8f89c2281330c8ede125ebdd02ee3eab
dbaldwin <dbaldwin@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2008-07-09 21:44:52 +0000

    Helper for displaying stop duration.

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -1,2 +1,10 @@
 module ReportsHelper
+  def minutes_to_hours(min)
+    if min < 60
+      (min % 60).to_s + " min"
+    else
+      hr = min / 60
+      hr.to_s + (hr == 1 ? " hr" : " hrs") + ", " + (min % 60).to_s + " min"
+    end
+  end
 end
