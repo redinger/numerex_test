866ae9768e59d94d12cc77d7b671f351cb7bb70a
dbaldwin <dbaldwin@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2008-07-12 23:47:26 +0000

    API key for mobile app

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,4 +25,4 @@ ActionMailer::Base.smtp_settings = {
   :port               => 25
 }
 
-Google_Maps_Api_Key = 'ABQIAAAAwBz7W3z4sedOzgKp8bfXIhQgkZGlVagNg2XBcspSTJffxa0_-xR_JUvw1p9eI5WUVX_JcKXKNjZHoQ'
+Google_Maps_Api_Key = 'ABQIAAAAb5FIzoTRib2_GSmlbkQ7mBQdy8buJCn4UCOrVn-gOC82x0BwgBQmZoveMS-zQ1aZv3RjGKOBbmDg9Q'
