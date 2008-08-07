783e55f7298c5deb0d9252bb4b50718a3b7eeb12
bappelt <bappelt@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2008-08-07 19:18:51 +0000

    added idle event to procs

--- config/database_procs.rb
+++ config/database_procs.rb
@@ -16,4 +16,18 @@ task :setup_db_procs do
   sudo "echo '* * * * * #{current_path}/stopreport.sh >> #{current_path}/log/stopreport.log 2>&1' >> oldcrontab"
   sudo "crontab -u #{user} oldcrontab"
   sudo "rm oldcrontab"
+  
+  begin  
+    sudo "crontab -u #{user} -l" do |channel, stream, data|
+      if !data.include?('no crontab for') #only get current contents if there is a crontab
+        sudo "crontab -u #{user} -l | grep -v 'idlereport.sh' > oldcrontab"
+      end
+    end
+  rescue
+  end
+
+  sudo "echo '* * * * * #{current_path}/idlereport.sh >> #{current_path}/log/idlereport.log 2>&1' >> oldcrontab"
+  sudo "crontab -u #{user} oldcrontab"
+  sudo "rm oldcrontab"
+  
 end
