2b3dbe029e936a4e4bfea34f24f692a7c98d044d
bappelt <bappelt@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2008-08-07 21:09:25 +0000

    do permission change on idlereports.sh

--- config/database_procs.rb
+++ config/database_procs.rb
@@ -1,5 +1,6 @@
 task :setup_db_procs do
   sudo "chmod a+x #{current_path}/stopreport.sh"
+  sudo "chmod a+x #{current_path}/idlereport.sh"
   sudo "mkdir -p /var/run/ublip_db"
   sudo "chown ublip:ublip /var/run/ublip_db"
   run "cd #{current_path} && rake setup_db_scripts RAILS_ENV=#{rails_env}"
