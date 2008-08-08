81819e26cb04ab31d69aea17334f08e3131681e6
amills <amills@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2008-08-08 03:23:44 +0000

    Reverting to previous version (problems with this file were build-system related, and unrelated to the content of this file).

--- test/functional/reports_controller_test.rb
+++ test/functional/reports_controller_test.rb
@@ -72,6 +72,50 @@ class ReportsControllerTest < Test::Unit::TestCase
     get :stop, {:id => 1, :start_time1=>{"month"=>"4", "day"=>"27", "year"=>"2007"}, :end_time1=>{"month"=>"7", "day"=>"1", "year"=>"2008"}}, {:user => users(:dennis), :account_id => users(:dennis).account_id}
     assert_equal 5, assigns(:record_count)
     assert_response :success
+=begin
+    pretend_now_is(Time.at(1185490000)) do
+      puts "now is:" + Time.now.to_s
+      get :stop, {:id=>"1", :t=>"1", :start_time1=>"Thu May 24 21:24:10 +0530 2004",:end_time1=>"Thu Jun 25 21:24:10 +0530 2008"}, {:user => users(:dennis), :account_id => users(:dennis).account_id } 
+      stops = assigns(:stops)
+      assert_equal 8, assigns(:record_count)
+      assert_response :success
+      assert_template "stop"
+
+      assert_equal 5, stops.size
+      
+      assert_equal -1, stops[0].duration
+      assert_equal Time.local(2007, "Jul", 26, 16, 55, 0), stops[0].created_at
+      
+      assert_nil stops[1].duration
+      assert_equal Time.local(2007, "Jul", 26, 16, 00, 0), stops[1].created_at
+      
+      assert_equal 3480, stops[2].duration
+      assert_equal Time.local(2007, "Jul", 26, 15, 0, 0), stops[2].created_at
+      
+      assert_equal 780, stops[3].duration
+      assert_equal Time.local(2007, "Jul", 26, 14, 48, 39), stops[3].created_at
+      
+      assert_equal Time.local(2007, "Jul", 26, 14, 37, 39), stops[4].created_at
+      assert_equal -1720.0, stops[4].duration
+      
+     
+      
+      get :stop, {:id=>"3", :t=>"1", :p => "2", :start_time1=>"Thu May 24 21:24:10 +0530 2004",:end_time1=>"Thu Jun 25 21:24:10 +0530 2008"}, { :user => users(:dennis), :account_id => users(:dennis).account_id }
+      stops = assigns(:stops)
+      
+      assert_equal 5, stops.size
+      
+      assert_equal -1, stops[0].duration
+      assert_equal Time.local(2007, "Jul", 26, 16, 55, 00), stops[0].created_at
+      assert_equal nil, stops[1].duration
+      assert_equal Time.local(2007, "Jul", 26, 16, 00, 00), stops[1].created_at
+      
+      assert_equal 3480.0, stops[2].duration
+      assert_equal Time.local(2007, "Jul", 26, 15, 00, 00), stops[2].created_at
+      
+    end
+
+=end
   end
   
   # Test geofence report
@@ -100,7 +144,7 @@ class ReportsControllerTest < Test::Unit::TestCase
     get :export, {:id => 6, :type => 'all', :start_time=>"Thu May 24 21:24:10 +0530 2008", :end_time=>"Thu Jun 26 21:24:10 +0530 2008"}, {:user => users(:dennis), :account_id => users(:dennis).account_id}
     assert_response :success
     assert_kind_of String, @response.body
-    #output = StringIO.new
+    output = StringIO.new
     #assert_nothing_raised { @response.body.call(@response, output) }
     #assert_equal csv_data, output.string
     # for stop events
@@ -111,11 +155,10 @@ class ReportsControllerTest < Test::Unit::TestCase
     assert_response :success
   end
   
-#  private
-#  def csv_data
-#    reading1 = readings(:reading24)
-#    reading2 = readings(:reading26)
-#    "latitude,longitude,address,speed,direction,altitude,event_type,note,when\r\n#{reading1.latitude},#{reading1.longitude},\"#{reading1.shortAddress}\",#{reading1.speed},#{reading1.direction},#{reading1.altitude},#{reading1.event_type},#{reading1.note},#{reading1.created_at}\r\n#{reading2.latitude},#{reading2.longitude},\"#{reading2.shortAddress}\",#{reading2.speed},#{reading2.direction},#{reading2.altitude},#{reading2.event_type},#{reading2.note},#{reading2.created_at}\r\n"
-#  end
+  private
+  def csv_data
+    reading1 = readings(:reading24)
+    reading2 = readings(:reading26)
+    "latitude,longitude,address,speed,direction,altitude,event_type,note,when\r\n#{reading1.latitude},#{reading1.longitude},\"#{reading1.shortAddress}\",#{reading1.speed},#{reading1.direction},#{reading1.altitude},#{reading1.event_type},#{reading1.note},#{reading1.created_at}\r\n#{reading2.latitude},#{reading2.longitude},\"#{reading2.shortAddress}\",#{reading2.speed},#{reading2.direction},#{reading2.altitude},#{reading2.event_type},#{reading2.note},#{reading2.created_at}\r\n"
+  end
 end
-
