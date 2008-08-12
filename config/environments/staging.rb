390e189608ce8be3576e1dc9743381e6e7e6a886
dbaldwin <dbaldwin@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2008-08-12 17:46:36 +0000

    Reverting

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,6 +1,4 @@
 # Settings specified here will take precedence over those in config/environment.rb
-# Explicitly setting staging ENV so that notifier.rb will pick this configuration up
-ENV['RAILS_ENV'] ||= 'staging'
 
 # The production environment is meant for finished, "live" apps.
 # Code is not reloaded between requests
