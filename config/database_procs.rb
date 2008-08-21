83bae6ef2cded76fedbfd34219227454ad0e0634
bappelt <bappelt@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2008-08-21 22:04:06 +0000

    added runtime report process

--- config/database_procs.rb
+++ config/database_procs.rb
@@ -31,4 +31,17 @@ task :setup_db_procs do
   sudo "crontab -u #{user} oldcrontab"
   sudo "rm oldcrontab"
   
+  begin  
+    sudo "crontab -u #{user} -l" do |channel, stream, data|
+      if !data.include?('no crontab for') #only get current contents if there is a crontab
+        sudo "crontab -u #{user} -l | grep -v 'runtimereport.sh' > oldcrontab"
+      end
+    end
+  rescue
+  end
+
+  sudo "echo '* * * * * #{current_path}/runtimereport.sh >> #{current_path}/log/runtimereport.log 2>&1' >> oldcrontab"
+  sudo "crontab -u #{user} oldcrontab"
+  sudo "rm oldcrontab"
+  
 end
