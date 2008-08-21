daf31b92aa468d8e2e21e78455a34e0b3d85db25
semmons <semmons@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2008-08-21 20:59:25 +0000

    test fixes

--- test/functional/admin_devices_controller_test.rb
+++ test/functional/admin_devices_controller_test.rb
@@ -31,7 +31,7 @@ class Admin::DevicesControllerTest < Test::Unit::TestCase
   
   def test_devices_table
     get :index, {}, get_user
-    assert_select "table tr", 9
+    assert_select "table tr", 10
   end
   
   def test_new_device
