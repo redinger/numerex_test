d43cf249e256e62656cefd341d451bd12c63d81a
dbaldwin <dbaldwin@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2008-08-25 23:01:34 +0000

    Route for device profiles, needed for SH servers

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ ActionController::Routing::Routes.draw do |map|
   map.connect 'admin/accounts/:action', :controller => 'admin/accounts'
   map.connect 'admin/users/:action', :controller => 'admin/users'
   map.connect 'admin/devices/:action', :controller => 'admin/devices'
+  map.connect 'admin/device_profiles/:action', :controller => 'admin/device_profiles'
   map.geofence 'geofence', :controller=>'geofence', :action=>'index'
   map.new 'geofence/new', :controller=>'geofence', :action=>'new'
   map.edit 'geofence/edit/:id', :controller=>'geofence', :action=>'edit', :id=>/\d+/    
