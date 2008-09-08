5e96713a2de340b8ccabc6ce1c9312af8788f627
dbaldwin <dbaldwin@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2008-09-08 22:17:18 +0000

    A first stab at testing with Shoulda

--- test/functional/ws_controller_test.rb
+++ test/functional/ws_controller_test.rb
@@ -5,14 +5,32 @@ require 'ws_controller'
 class WsController; def rescue_action(e) raise e end; end
 
 class WsControllerTest < Test::Unit::TestCase
-  def setup
-    @controller = WsController.new
-    @request    = ActionController::TestRequest.new
-    @response   = ActionController::TestResponse.new
-  end
-
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  
+  context "A WS instance" do
+    setup do
+      @controller = WsController.new
+      @request    = ActionController::TestRequest.new
+      @response   = ActionController::TestResponse.new
+    end
+        
+    context "without any parameters" do
+      should "respond with 200 OK" do
+        get :index
+        assert_response :success
+      end
+      
+      should "send return default message" do
+        get :index
+        assert_equal @response.body, "We're sorry, this device does not exist"
+      end
+    end
+    
+    context "with valid parameters" do
+      should "return success message" do
+        get :index, {:imei => "1234", :lat => 1, :lng => 2, :spd => 3, :dir => 4, :alt => 5}
+        assert_equal @response.body, "Success"
+      end
+    end
+    
   end
 end
