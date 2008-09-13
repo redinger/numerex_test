e0c91e5a21142735ec1b51c118997f0df2a1bcbe
semmons <semmons@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2008-09-13 20:26:11 +0000

    first pass at scripts and namespace fixes

--- app/controllers/enfora/command_request_controller.rb
+++ app/controllers/enfora/command_request_controller.rb
@@ -13,10 +13,49 @@ class Enfora::CommandRequestController < Enfora::CommonController
     @error = $!.to_s
     @command_request ||= Enfora::CommandRequest.new
   end
+
+  def upload
+    @command_request = Enfora::CommandRequest.find(params[:id])
+    if request.post?
+      command_found = false
+      @expect = 'OK'
+      @script = []
+      params[:file].each do | line |
+        line.strip!
+        entry = {:line => line}
+        entry[:comment] = line[/\#.*/]
+        line = line[/^[^#]*/].strip if entry[:comment]
+        if line.blank?
+          entry[:blank] = true
+        elsif line[0,1] == '!'
+          @expect = line[1,line.length - 1].upcase
+          next
+        else
+          entry[:command] = line
+          entry[:expect] = @expect
+          command_found = true
+        end
+        @script.push(entry)
+      end
+      return if command_found
+      @script = nil
+      @error = "No commands found"
+    end
+  rescue
+    @error = $!.to_s
+    @command_request ||= Enfora::CommandRequest.new
+  end
+  
+  def script
+    @command_request = Enfora::CommandRequest.find(params[:id])
+  rescue
+    @error = $!.to_s
+    @command_request ||= Enfora::CommandRequest.new
+  end
   
   def list
-    @device = Device.find(params[:id]) if params[:id]
-    @device ||= Device.new
+    @device = Enfora::Device.find(params[:id]) if params[:id]
+    @device ||= Enfora::Device.new
     conditions = "device_id = #{@device.id}" if @device.id
     @command_requests = Enfora::CommandRequest.find(:all,:conditions => conditions,:order => "start_date_time desc")
   end
