ed2f25b9c130455bcd449515e0da7b8bf6e57dc9
dbaldwin <dbaldwin@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2008-10-02 20:27:06 +0000

    Adding group drop-down to device index

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -144,13 +144,15 @@ class DevicesController < ApplicationController
   end
 
   def index
-     if session[:gmap_value]=="all" 
-         @devices = Device.get_devices(session[:account_id]) # Get devices associated with account    
-     elsif session[:gmap_value]=="default"
-         @devices =Device.find(:all, :conditions=>['account_id=? and group_id is NULL and provision_status_id=1',session[:account_id]], :order=>'name')                     
-     else
-         @devices = Device.find(:all, :conditions=>['account_id=? and group_id =? and provision_status_id=1',session[:account_id],session[:gmap_value]], :order=>'name')
-     end    
+    session[:gmap_value] = params[:group_id] if params[:group_id]
+    @groups=Group.find(:all, :conditions=>['account_id=?',session[:account_id]], :order=>'name')
+    if session[:gmap_value]=="all" 
+      @devices = Device.get_devices(session[:account_id]) # Get devices associated with account    
+    elsif session[:gmap_value]=="default"
+      @devices =Device.find(:all, :conditions=>['account_id=? and group_id is NULL and provision_status_id=1',session[:account_id]], :order=>'name')                     
+    else
+      @devices = Device.find(:all, :conditions=>['account_id=? and group_id =? and provision_status_id=1',session[:account_id],session[:gmap_value]], :order=>'name')
+    end    
   end
 
 
