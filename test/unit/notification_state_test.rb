9a15dbc243602af5c9d34b10c545c814ec2443b5
semmons <semmons@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2008-10-02 16:44:19 +0000

    notification state unit test

--- test/unit/notification_state_test.rb
+++ test/unit/notification_state_test.rb
@@ -1,8 +1,35 @@
-require 'test_helper'
+require File.dirname(__FILE__) + '/../test_helper'
 
 class NotificationStateTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  fixtures :notification_states,:readings
+
+  def test_precise_ranges
+    all_readings_count = Reading.count(:all)
+    
+    NotificationState.instance.begin_reading_bounds
+    
+    Reading.create! # this reading should NOT be included
+    
+    bound_readings_count = Reading.count(:all,:conditions => NotificationState.instance.reading_bounds_condition)
+    
+    NotificationState.instance.end_reading_bounds
+    
+    assert_equal all_readings_count,bound_readings_count # only the initial set should be included
+    
+    NotificationState.instance.begin_reading_bounds
+    
+    bound_readings_count = Reading.count(:all,:conditions => NotificationState.instance.reading_bounds_condition)
+    
+    NotificationState.instance.end_reading_bounds
+    
+    assert_equal 1,bound_readings_count # only the previously-created reading should be found
+    
+    begin
+      Reading.count(:all,:conditions => NotificationState.instance.reading_bounds_condition)
+      fail "Exception expected"
+    rescue
+      # do nothing, exception expected
+    end
   end
+  
 end
