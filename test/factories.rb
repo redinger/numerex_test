92037589421c050d52f68fff30b2dff276e14451
bappelt <bappelt@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2008-10-02 21:26:34 +0000

    added device profile factory

--- test/factories.rb
+++ test/factories.rb
@@ -18,6 +18,10 @@ Factory.sequence :imei do |n|
   "#{n}"
 end
 
+Factory.sequence :profile_name do |n|
+  "profile#{n}"
+end
+
 Factory.define :user do |u|
   u.first_name { Factory.next(:name) }
   u.last_name "Smith"
@@ -53,4 +57,10 @@ end
 Factory.define :group_notification do |gn|
   gn.group {|group| group.association(:group)}
   gn.user {|user| user.association(:user)}
+end
+
+Factory.define :device_profile do |dp|
+  dp.name {Factory.next(:profile_name)}
+  dp.gpio1_labels "gpio1\tlow\thigh\tlow_notice\thigh_notice"
+  dp.gpio2_labels "gpio2\tlow\thigh\tlow_notice\thigh_notice"
 end
