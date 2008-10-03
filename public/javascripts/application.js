6ddde7e8885fdd2b209e1a99d060dce032ff6aab
dbaldwin <dbaldwin@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2008-10-03 21:54:16 +0000

    Updating JS to work in both local mode with port and deployed mode without port

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -4,24 +4,24 @@ function select_action(obj,from)
   var selected_text = document.getElementById("group_type").options[Index].text;              
     if (selected_text == "New group")
     {
-         szNewURL = "http://"+document.location.hostname+"/devices/new_group";         
+         szNewURL = "/devices/new_group";         
         window.location.href=szNewURL;            
     }
     else if (selected_text == "Edit groups")
     {
-         szNewURL = "http://"+document.location.hostname+"/devices/groups"; 
+         szNewURL = "/devices/groups"; 
         window.location.href=szNewURL;                
     }
     else
     {
       if (from=='from_devices')
       {         
-           szNewURL = "http://"+document.location.hostname+"/devices?group_type="+document.getElementById('group_type').value;         
+           szNewURL = "/devices?group_type="+document.getElementById('group_type').value;         
            window.location.href=szNewURL;                                
       }
       else 
       {            
-           szNewURL = "http://"+document.location.hostname+":3000/home/show_devices?group_type="+document.getElementById('group_type').value+"&frm=" + from;          
+           szNewURL = "/home/show_devices?group_type="+document.getElementById('group_type').value+"&frm=" + from;          
            document.location.href=szNewURL;                                                  
        }  
     }
