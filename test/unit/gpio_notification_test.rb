d2643a1b7ba6b341a53135a43e7a097a806d3830
bappelt <bappelt@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2008-10-03 17:56:52 +0000

    added GPIO2 tests

--- test/unit/gpio_notification_test.rb
+++ test/unit/gpio_notification_test.rb
@@ -1,31 +1,32 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
-module MockReadingNotifier
-  def Notifier.send_notify_reading_to_users(action,reading)
-    @test.record_notification(action,reading)
-  end
-  def set_test(test)
-    @test = test
-  end
-end
-
 
 class GPIONotificationTest < Test::Unit::TestCase
   
-public
-def record_notification(action,reading)
-  @notified_actions.push(action)
-end
-
+  def record_notification(action,reading)
+    @notified_actions.push(action)
+  end
+  
   context "A GPIO notification" do
     setup do
-      Notifier.extend(MockReadingNotifier)
+      
+      #mock out send method for testing
+      Notifier.class_eval do
+        def Notifier.send_notify_reading_to_users(action,reading)
+          @test.record_notification(action,reading)
+        end
+        def Notifier.set_test(test)
+          @test = test
+        end
+      end
+      
       Notifier.set_test(self)
       @notified_actions = Array.new
       @notified_readings = Array.new
     end
     
     teardown do
+      #reload notifier to remove mocked method
       Object.class_eval do
         remove_const :Notifier.to_s
         load "notifier.rb"
@@ -39,20 +40,45 @@ end
       end
       
       should "notify on GPIO1 L/H" do
-        Factory.create(:reading, :device => @device, :gpio1 => false)
-        NotificationState.instance.begin_reading_bounds
-        Notifier.send_gpio_notifications(Logger.new("logger"))
-        NotificationState.instance.end_reading_bounds
-        Factory.create(:reading, :device => @device, :gpio1 => true)
-        NotificationState.instance.begin_reading_bounds
-        Notifier.send_gpio_notifications(Logger.new("logger"))
-        NotificationState.instance.end_reading_bounds
+        create_reading(@device, false, false)
+        create_reading(@device, true, false)
         assert_equal 1, @notified_actions.size
         assert @notified_actions.include?(@profile.gpio1_high_notice)
       end
+      
+      should "not notify on GPIO2 L/H" do
+        create_reading(@device, false, false)
+        create_reading(@device, false, true)
+        assert_equal 0, @notified_actions.size
+      end
     end
     
-    
+    context "with a profile watching GPIO2" do
+       setup do
+        @profile = Factory.create(:device_profile, :watch_gpio2 => true)
+        @device = Factory.create(:device, :profile => @profile)
+      end
+      
+      should "notify on GPIO2 L/H" do
+        create_reading(@device, false, false)
+        create_reading(@device, false, true)
+        assert_equal 1, @notified_actions.size
+        assert @notified_actions.include?(@profile.gpio1_high_notice)
+      end
+      
+      should "not notify on GPIO1 L/H" do
+        create_reading(@device, false, false)
+        create_reading(@device, true, false)
+        assert_equal 0, @notified_actions.size
+      end
+    end
+  end
+  
+  def create_reading(device, gpio1, gpio2)
+    Factory.create(:reading, :device => device, :gpio1 => gpio1, :gpio2 => gpio2)
+    NotificationState.instance.begin_reading_bounds
+    Notifier.send_gpio_notifications(Logger.new("logger"))
+    NotificationState.instance.end_reading_bounds
   end
   
 end
