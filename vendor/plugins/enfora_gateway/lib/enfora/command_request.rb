d84df0533f9b6c8950a262383f6e7a2cf2436fdf
dbaldwin <dbaldwin@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2008-10-30 20:16:17 +0000

    Removing Enfora outbound stuff.

--- vendor/plugins/enfora_gateway/lib/enfora/command_request.rb
+++
@@ -1,7 +0,0 @@
-require 'enfora/device'
-
-class Enfora::CommandRequest < ActiveRecord::Base
-  set_table_name "outbound"
-  
-  belongs_to :device
-end
--- vendor/plugins/enfora_gateway/tasks/enfora_gateway_tasks.rake
+++
@@ -1,4 +0,0 @@
-# desc "Explaining what the task does"
-# task :enfora_gateway do
-#   # Task goes here
-# end
