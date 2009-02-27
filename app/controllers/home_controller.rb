a7ee67ba9a45c6e30115bae3b2a1c41e3e75bd82
semmons <semmons@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2009-02-27 03:09:44 +0000

    change Group to Fleet, Device to Vehicle, and other RSCCOMM requests

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,7 @@ class HomeController < ApplicationController
     @all_groups=Group.find(:all, :conditions=>['account_id=?',session[:account_id]], :order=>'name', :include=>[:devices])    
     @device_count = Device.count(:all, :conditions => ['provision_status_id = 1 and account_id = ?', session[:account_id]])
     @default_devices=Device.find(:all, :conditions=>['account_id=? and group_id is NULL and provision_status_id=1',session[:account_id]], :order=>'name', :include => :profile)                           
+    @all_devices=Device.find(:all, :conditions=>['account_id=? and provision_status_id=1',session[:account_id]], :order=>'name', :include => :profile)                           
     assign_the_selected_group_to_session 
   end
   
@@ -18,8 +19,16 @@ class HomeController < ApplicationController
     index 
   end
   
-  def show_devices                 
-    params[:group_type].nil? || params[:group_type]=="all" ?  session[:group_value]="all" : session[:group_value] = params[:group_type]
+  def show_devices
+    if params[:group_type]
+      if (group_id = params[:group_type].to_i) < 0
+        session[:group_value] = 'all'
+        session[:home_device] = (-group_id).to_s
+      else
+        session[:group_value] = params[:group_type]
+      end
+    end
+    session[:home_device] = nil unless session[:group_value] == 'all'
     if params[:frm]=='from_statistics'
       @from_statistics  = true        
       redirect_to :action=>'statistics'
