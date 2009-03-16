2f76146a8e9720d21f41c5d6cacb1f8fd20229fc
semmons <semmons@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2009-03-16 18:09:51 +0000

    don't process 0,0 or nil,nil readings for mileage

--- app/models/trip_event.rb
+++ app/models/trip_event.rb
@@ -9,12 +9,12 @@ class TripEvent < ActiveRecord::Base
     self.idle = IdleEvent.sum(:duration,:conditions => ['device_id = ? and created_at between ? and ?',self.device_id,reading_start.created_at,reading_stop.created_at],:order => 'created_at')
 
     self.distance = 0
-    last_latitude,last_longitude = 0,0
-    readings = Reading.find(:all,:conditions => ['device_id = ? and created_at between ? and ?',self.device_id,reading_start.created_at,reading_stop.created_at],:order => 'created_at')
-    for reading in readings
-      next_distance = TripEvent.connection.execute("select distance(latitude,longitude,#{last_latitude},#{last_longitude}) from readings where id = #{reading.id} and latitude != 0 and longitude != 0").fetch_row if reading.latitude != 0 and reading.longitude != 0 and last_latitude != 0 and last_longitude != 0
+    last_reading = nil
+    readings = Reading.find(:all,:conditions => ['device_id = ? and created_at between ? and ? and latitude != 0 and longitude != 0 and latitude is not null and longitude is not null',self.device_id,reading_start.created_at,reading_stop.created_at],:order => 'created_at')
+    for next_reading in readings
+      next_distance = TripEvent.connection.execute("select distance(latitude,longitude,#{last_reading.latitude},#{last_reading.longitude}) from readings where id = #{next_reading.id} and latitude != 0 and longitude != 0").fetch_row if last_reading
       self.distance += next_distance[0].to_f if next_distance
-      last_latitude,last_longitude = reading.latitude,reading.longitude
+      last_reading = next_reading
     end
     
     save!
